a72d19243d7005a5faf0510e25fb4992ffe454b7
eric12076 <eric12076@gmail.com>
2022-11-02 05:35:22 +0800

    智障樣板問題

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap 5 CDN Link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
@@ -22,3 +24,4 @@
 </body>
 
 </html>
+
